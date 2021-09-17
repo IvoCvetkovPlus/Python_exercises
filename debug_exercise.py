4e2f8769903c352feb7632e3d87fba42a4909087
IvoCvetkovPlus <89647414+IvoCvetkovPlus@users.noreply.github.com>
2021-09-17 12:23:43 +0300

    Create debug_exercise.py

---
+++ debug_exercise.py
@@ -0,0 +1,19 @@
+# ---------------------------------
+# Bugged 
+# --------------------------------
+def add_underscores(word):
+    new_word = "_"
+    for i in range(0, len(word)):
+        new_word = word[i] + "_"
+    return new_word
+phrase = "hello"
+print(add_underscores(phrase))
+# ---------------------------------
+#  Clear
+def add_underscores(word):
+    new_word = "_"
+    for i in range(0, len(word)):
+        new_word =new_world + word[i] + "_"
+    return new_word
+phrase = "hello"
+print(add_underscores(phrase))
